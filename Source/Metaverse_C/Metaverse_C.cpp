424686a57e5baf8959c713c037b31c3dc1069ede
DesmondWork <desmond.adcreate73@gmail.com>
2023-08-29 17:57:03 +0800

    Resend

---
+++ Source/Metaverse_C/Metaverse_C.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Metaverse_C.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Metaverse_C, "Metaverse_C" );
---
+++ Source/Metaverse_C/Metaverse_CGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Metaverse_CGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METAVERSE_C_API AMetaverse_CGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
